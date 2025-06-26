4d71b2727a482726855cd9e595e0bff90bf8845d
Milwayky <vyryshevaalisa@gmailcom>
2025-06-26 13:25:41 +0600

    Реализован метод IsPalindrome

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace task01
+{
+    public static class StringExtensions
+    {
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrWhiteSpace(input)) return false;
+            string str = input.ToLower();
+            List<char> list = new List<char>();
+            foreach (char c in str)
+            {
+                if (!char.IsWhiteSpace(c) && !char.IsPunctuation(c))
+                {
+                    list.Add(c);
+                }
+            }
+            string cleaned = new string(list.ToArray());
+            list.Reverse();
+            string reversed = new string(list.ToArray());
+            return cleaned == reversed;
+        }
+    }
+}
